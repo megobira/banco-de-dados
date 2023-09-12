be4396c6a098f4e406c1aa47c00365e06933df18
megobira <102704350+megobira@users.noreply.github.com>
2023-09-12 20:08:13 -0300

    Exercício_04

--- lista01_bancodedados.sql
+++ lista01_bancodedados.sql
@@ -8,3 +8,8 @@ SELECT nome FROM autores WHERE nascimento < '1900-01-01';
 SELECT livros.titulo, autores.nome FROM livros
 INNER JOIN autores ON livros.autor_id = autores.id
 WHERE autores.nome = 'J.K. Rowling';
+
+-- 04
+SELECT alunos.nome, matriculas.curso FROM alunos
+INNER JOIN matriculas ON alunos.id = matriculas.aluno_id
+WHERE matriculas.curso = 'Engenharia de Software';
