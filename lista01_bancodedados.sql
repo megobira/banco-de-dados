f5a1c6456de60b7df2ce0eadf5fed55dc06db62e
megobira <102704350+megobira@users.noreply.github.com>
2023-09-12 20:02:19 -0300

    Exercício_03

--- lista01_bancodedados.sql
+++ lista01_bancodedados.sql
@@ -3,3 +3,8 @@ SELECT titulo FROM livros;
 
 -- 02 
 SELECT nome FROM autores WHERE nascimento < '1900-01-01';
+
+-- 03
+SELECT livros.titulo, autores.nome FROM livros
+INNER JOIN autores ON livros.autor_id = autores.id
+WHERE autores.nome = 'J.K. Rowling';
