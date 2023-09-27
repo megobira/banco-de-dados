942c0ca2d8378358e8d3f2f3f8bdc98705da27d0
megobira <102704350+megobira@users.noreply.github.com>
2023-09-26 22:17:08 -0300

    Livros por Categoria

--- lista02.sql
+++ lista02.sql
@@ -7,3 +7,17 @@ END; //
 DELIMITER ;
 
 CALL sp_ListarAutores();
+
+-- 02 
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END; //
+DELIMITER ;
+
+CALL sp_LivrosPorCategoria ('Romance');
+CALL sp_LivrosPorCategoria ('Autoajuda');
