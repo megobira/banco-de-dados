4ad7b66e3aa2f34491e79ab1959e47b1d1a315a5
megobira <102704350+megobira@users.noreply.github.com>
2023-09-26 22:52:08 -0300

    Livros e Seus Autores

--- lista02.sql
+++ lista02.sql
@@ -137,3 +137,16 @@ END //
 DELIMITER ;
 
 CALL sp_ListarAutoresNovo();
+
+-- 10
+DELIMITER //
+CREATE PROCEDURE sp_LivrosESeusAutores()
+BEGIN
+    SELECT Livro.Titulo, CONCAT(Autor.Nome, ' ', Autor.Sobrenome) AS 'Nome do Autor'
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
+END //
+DELIMITER ;
+
+CALL sp_LivrosESeusAutores();
